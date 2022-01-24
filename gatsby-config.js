932ed61ef24fd8072c56717c767a140c860205d1
Jacob Sidford <jacob.sidford@gmail.com>
2022-01-24 22:36:20 +1100

    added tailwind css

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,6 +14,7 @@ module.exports = {
   plugins: [
     `gatsby-plugin-image`,
     `gatsby-plugin-gatsby-cloud`,
+    `gatsby-plugin-postcss`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
