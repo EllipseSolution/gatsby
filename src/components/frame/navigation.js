06481857863d8886ffab65b84b9470fbfc8e77b7
Jacob Sidford <jacob.sidford@gmail.com>
2022-01-26 19:45:13 +1100

    moved footer & nav to components/frame

--- src/components/frame/navigation.js
+++ src/components/frame/navigation.js
@@ -11,54 +11,6 @@ const Layout = ({ location, title, children }) => {
     return () => window.removeEventListener("scroll", onScroll)
   }, [])
 
-  // if (scrollpos > 10) {
-  //   header.classList.add("bg-white");
-  //   navaction.classList.remove("bg-white");
-  //   navaction.classList.add("gradient");
-  //   navaction.classList.remove("text-gray-800");
-  //   navaction.classList.add("text-white");
-  //   //Use to switch toggleColour colours
-  //   for (var i = 0; i < toToggle.length; i++) {
-  //     toToggle[i].classList.add("text-gray-800");
-  //     toToggle[i].classList.remove("text-white");
-  //   }
-  //   header.classList.add("shadow");
-  //   navcontent.classList.remove("bg-gray-100");
-  //   navcontent.classList.add("bg-white");
-  // } else {
-  //   header.classList.remove("bg-white");
-  //   navaction.classList.remove("gradient");
-  //   navaction.classList.add("bg-white");
-  //   navaction.classList.remove("text-white");
-  //   navaction.classList.add("text-gray-800");
-  //   //Use to switch toggleColour colours
-  //   for (var i = 0; i < toToggle.length; i++) {
-  //     toToggle[i].classList.add("text-white");
-  //     toToggle[i].classList.remove("text-gray-800");
-  //   }
-
-  //   header.classList.remove("shadow");
-  //   navcontent.classList.remove("bg-white");
-  //   navcontent.classList.add("bg-gray-100");
-  // }
-
-  // const rootPath = `${__PATH_PREFIX__}/`
-  // let header
-
-  // if (isRootPath) {
-  //   header = (
-  //     <h1 className="main-heading">
-  //       <Link to="/">{title}</Link>
-  //     </h1>
-  //   )
-  // } else {
-  //   header = (
-  //     <Link className="header-link-home" to="/">
-  //       {title}
-  //     </Link>
-  //   )
-  // }
-
   return (
     <nav
       id="header"
